2fa9ea2a664ca3c141eef325cc8c0a27b140ea70
Mariana Rodrigues <marianarodrigues.mr116@gmail.com>
2026-01-29 20:00:07 +0000

    Update licenses and deployment script

--- tools/deploy_sca.sh
+++ tools/deploy_sca.sh
@@ -1,1 +1,3 @@
-cd ..; mvn clean install; java -jar target/sca-0.0.1-SNAPSHOT.jar
+export SPRING_PROFILES_ACTIVE=dev
+mvn clean install
+nohup java -jar target/sca-0.0.1-SNAPSHOT.jar > nohup_SCA.out 2>&1 &
